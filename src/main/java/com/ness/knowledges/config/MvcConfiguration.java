d4296b3beaa0a7b88aae50cf261f8510c9891824
Valentin Banshchikov <valentin.banshchikov@ness.com>
2016-06-21 14:28:59 +0200

    Init repo

---
+++ src/main/java/com/ness/knowledges/config/MvcConfiguration.java
@@ -0,0 +1,48 @@
+package com.ness.knowledges.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@Configuration
+@ComponentScan(basePackages="com.ness.knowledges")
+@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode("HTML5");
+		return resolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine(ServletContextTemplateResolver resolver) {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(resolver);
+		return engine;
+	}
+	
+	@Bean
+	public ViewResolver viewResolver(SpringTemplateEngine templateEngine){
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine);
+		return viewResolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	
+}
